a4cdeb53c9c6d0b47feb9ac49dd84b3d3931944b
Piotr Praszmo <ppraszmo@gmail.com>
2014-04-10 21:31:59 +0200

    init

---
+++ src/Board.hx
@@ -0,0 +1,132 @@
+package ;
+
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.events.KeyboardEvent;
+import nape.geom.Vec2;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.shape.Polygon;
+import nape.space.Space;
+import nape.util.BitmapDebug;
+
+class Board extends Sprite
+{
+
+	static var size = 107;
+	static var pad = size + 14;
+	static var offset = 15;
+	
+	var space:Space;
+	var pieces:List<Piece>;
+	
+	public function new() {
+		super();
+		
+		pieces = new List();
+		space = new Space(new Vec2(0, 0));
+		
+		graphics.beginFill(0xbbada0);
+		graphics.drawRoundRect(0, 0, 500, 500, 12, 12);
+		graphics.endFill();
+		
+		for (x in 0...4) {
+			for (y in 0...4) {
+				graphics.beginFill(0xccc0b3);
+				graphics.drawRoundRect(offset + pad * x, offset + pad * y, size, size, 6, 6);
+				graphics.endFill();
+			}
+		}
+		
+		addRandom();
+
+		var wall = new Body(BodyType.STATIC);
+		var f = function(shape:Polygon) {
+			shape.filter.collisionGroup = -1;
+			shape.filter.collisionMask = -1;
+			wall.shapes.add(shape);
+		}	
+		f(new Polygon(Polygon.rect( -500, -500+12, 1500, 500)));
+		f(new Polygon(Polygon.rect( -500, 500-12, 1500, 500)));
+		f(new Polygon(Polygon.rect(-500+12, -500, 500, 1500)));
+		f(new Polygon(Polygon.rect(500 - 12, -500, 500, 1500)));
+		wall.space = space;
+
+	}
+	
+	public function init() {
+		addEventListener(Event.ENTER_FRAME, tick);
+		stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+		stage.addEventListener(KeyboardEvent.KEY_UP, onKeyUp);
+	}
+	
+	public function onKeyDown(e:KeyboardEvent) {
+		var power = 800;
+		switch(Std.int(e.keyCode)) {
+			case 37:
+				space.gravity.setxy(-power, 0);
+			case 38:
+				space.gravity.setxy(0, -power);
+			case 39:
+				space.gravity.setxy(power, 0);
+			case 40:
+				space.gravity.setxy(0, power);
+			default:
+				trace(e.keyCode);
+		}
+	}
+	
+	public function onKeyUp(e:KeyboardEvent) {
+		space.gravity.setxy(0, 0);
+		addRandom();
+	}
+	
+	public function addRandom() {
+		var x = Std.int(Math.random() * 4);
+		var y = Std.int(Math.random() * 4);
+		addPiece(x, y, Math.random() > 0.9?2:1);
+	}
+	
+	public function tick(?_) {
+		space.step(1.0 / 60.0);
+		
+		for ( a in pieces ) {
+			for ( b in pieces ) {
+				if(!a.removed && !b.removed) {
+					if ( a!= b && Vec2.distance(a.body.position, b.body.position) < 10.0) {
+						combinePiece(a, b);
+					}
+				}
+			}
+		}
+		for ( piece in pieces ) {
+			piece.tick();
+		}
+	}
+	
+	public function removePiece(p:Piece) {
+		p.removed = true;
+		p.body.space = null;
+		pieces.remove(p);
+		removeChild(p);
+	}
+	
+	public function combinePiece(a:Piece, b:Piece) {
+		removePiece(a);
+		removePiece(b);
+		var piece = new Piece(a.n+1);
+		piece.body.position.set(a.body.position.add(b.body.position).mul(0.5));
+		piece.body.rotation = a.body.rotation;
+		piece.body.space = space;
+		pieces.add(piece);
+		addChild(piece);
+	}
+	
+	public function addPiece(x, y, n) {
+		var piece = new Piece(n);
+		piece.body.position.setxy( x * pad + offset + size/2, y * pad + offset + size/2);
+		piece.body.space = space;
+		pieces.add(piece);
+		addChild(piece);
+	}
+}
---
+++ src/Piece.hx
@@ -0,0 +1,71 @@
+package ;
+
+import flash.display.Shape;
+import flash.display.Sprite;
+import flash.text.TextField;
+import flash.text.TextFieldAutoSize;
+import flash.text.TextFormat;
+import flash.text.TextFormatAlign;
+import flash.text.AntiAliasType;
+import nape.phys.Body;
+import nape.phys.BodyType;
+import nape.shape.Polygon;
+import openfl.Assets;
+
+class Piece extends Sprite
+{
+
+	public var n:Int;
+	public var body:Body;
+	public var removed:Bool;
+	
+	private function min(a,b) {
+		return a < b?a:b;
+	}
+	
+	public function new(n) 
+	{
+		var colors = [0, 0xeee4da, 0xede0c8, 0xf2b179, 0xf59563, 0xf67c5f, 0xf65e3b, 0xedcf72, 0xedcc61, 0xedc850, 0xedc53f, 0xedc22e, 0x3c3a32];
+		var fontColor = [0, 0x776e65, 0x776e65, 0xffffff];
+		var fontSize = [0, 55, 55, 55, 55, 55, 55, 45, 45, 45, 35];
+		var size = 107;
+		
+		super();
+		this.n = n;
+		graphics.beginFill(colors[min(n,colors.length-1)]);
+		graphics.drawRoundRect(-size/2, -size/2, size, size, 6, 6);
+		graphics.endFill();
+		
+		var format = new TextFormat();
+		format.font = Assets.getFont("fonts/FreeSans.ttf").fontName;
+		
+		format.size = fontSize[min(n,fontSize.length-1)];
+		format.color = fontColor[min(n,fontColor.length-1)];
+		format.align = TextFormatAlign.CENTER;
+		
+		var text = new TextField();
+		text.defaultTextFormat = format;
+		text.text = Std.string(1 << n);
+		text.selectable = false;
+		text.autoSize = TextFieldAutoSize.LEFT;
+		text.embedFonts = true;
+		addChild(text);
+		text.x = (width - text.width) / 2 - width/2;
+		text.y = (height - text.height) / 2 - height/2;
+
+		body = new Body(BodyType.DYNAMIC);
+		var shape = new Polygon(Polygon.box(109, 109));
+		shape.filter.collisionMask = (1 << n);
+		shape.filter.collisionGroup = ~ (1 << n);
+		body.shapes.add(shape);
+		
+		
+	}
+	
+	public function tick() {
+		x = body.position.x;
+		y = body.position.y;
+		rotation = 180 * body.rotation / Math.PI;
+	}
+	
+}
